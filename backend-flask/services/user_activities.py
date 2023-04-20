fd6bc8ff29701d068d967ae27e011b2570cd38d4
StavrosCaptain <stavrstavr16@yahoo.gr>
2023-04-20 08:43:59 +0000

    implement cloudwatch logs

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -2,8 +2,8 @@ from datetime import datetime, timedelta, timezone
 from aws_xray_sdk.core import xray_recorder
 class UserActivities:
   def run(user_handle):
-
-    segment = xray_recorder.begin_segment('user_activities')
+    # x-ray
+    # segment = xray_recorder.begin_segment('user_activities')
 
     model = {
       'errors': None,
@@ -27,10 +27,10 @@ class UserActivities:
 
     subsegment = xray_recorder.begin_subsegment('mock-data')
     # xray ---
-    dict = {
-      "now": now.isoformat(),
-      "results-size": len(model['data'])
-    }
-    subsegment.put_metadata('key', dict, 'namespace')
+    #dict = {
+    #  "now": now.isoformat(),
+    #  "results-size": len(model['data'])
+    #}
+    # subsegment.put_metadata('key', dict, 'namespace')
 
     return model
