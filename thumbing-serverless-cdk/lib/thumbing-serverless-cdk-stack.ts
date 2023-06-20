02fd8c5bdf9467266f436797988b9e040ed85a30
StavrosCaptain <stavrstavr16@yahoo.gr>
2023-06-20 06:31:56 +0000

    wip

--- thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
+++ thumbing-serverless-cdk/lib/thumbing-serverless-cdk-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3n from 'aws-cdk-lib/aws-s3-notifications';
 import { Construct } from 'constructs';
 import * as dotenv from 'dotenv';
 
@@ -26,6 +27,9 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
 
     const bucket = this.createBucket(bucketName);
     const lambda = this.createLambda(functionPath, bucketName, folderInput, folderOutput);
+  
+    this.createS3NotifyToLambda(folderInput,lambda,bucket)
+
   }
 
   createBucket(bucketName: string): s3.IBucket {
@@ -52,4 +56,12 @@ export class ThumbingServerlessCdkStack extends cdk.Stack {
     return lambdaFunction;
   }
 
+  createS3NotifyToLambda(prefix: string, Lambda: lambda.IFunction, bucket: s3.IBucket): void {
+    const destination = new s3n.LambdaDestination(Lambda);
+    bucket.addEventNotification(
+      s3.EventType.OBJECT_CREATED_PUT,
+      destination,
+      {prefix: prefix} // folder to contain the original images
+    )
+  }
 }
